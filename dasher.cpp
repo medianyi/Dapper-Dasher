86fb3d4cfbe1ad362193c69cc838d850b14a08d5
unknown <vladimirmedianyi@gmail.com>
2022-09-26 17:43:38 +0300

    start program dasher game

---
+++ dasher.cpp
@@ -0,0 +1,38 @@
+#include "raylib.h"
+
+int main()
+{
+    // window dimensions
+    const int windowWidth{512};
+    const int windowHeight{380};
+    // initialize the window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    //    Rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight  - height};
+    int velocity{0};
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+        {
+            // start drawing
+            BeginDrawing();
+            ClearBackground(WHITE);
+
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                velocity = -10;
+            }
+            posY += velocity;
+
+            DrawRectangle(windowWidth/2, posY, width, height, BLUE);
+
+            // stop drawing
+            EndDrawing();
+        }
+    CloseWindow();
+
+}
